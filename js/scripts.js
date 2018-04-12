8f2d30b4874a335699ba250202b4622363e482b7
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-12 17:05:22 +0200

    Change js file and html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,16 @@
-var a = prompt("1")
-var b = prompt("1")
+var a = ("1")
+var b = ("1")
 var value = ("-2")
-console.log("-2")
+console.log("(a * a) - (2 * a * b) - (b * b) = -2")
+
+if (value > 0 ) {
+	document.write("wynik dodatni");
+} else if (value < 0) {
+	document.write("wynik ujemny");
+}  
+
+if (value = 0) {
+	document.write("wynik równy zero");
+} else if (value != 0) {
+	document.write("wynik nierówny zero");
+}
