68194533b518397223b8e2139a3f8af24292428b
Maja Jelenik <maja.jelenik@vp.pl>
2018-04-13 11:30:46 +0200

    Change js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,14 @@
-var a = ("1")
-var b = ("1")
-var value = ("-2")
-console.log("(a * a) - (2 * a * b) - (b * b) = -2")
+var a = prompt("Podaj a");
+var b = prompt("Podaj b");
+var value = (a * a) - (2 * a * b) - (b * b);
+console.log(value);
 
-if (value > 0 ) {
+if (value > 0) {
 	document.write("wynik dodatni");
 } else if (value < 0) {
 	document.write("wynik ujemny");
-}  
-
-if (value = 0) {
-	document.write("wynik równy zero");
-} else if (value != 0) {
-	document.write("wynik nierówny zero");
+} else if (isNaN(value)) {
+	document.write("wynik jest NaN");
+} else {
+	document.write("wynik jest równy zero");
 }
